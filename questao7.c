455cea737f7f87d33fa0d4a18b21210ee731e618
Luiz Sales <luiz.salessi2025@gmail.com>
2026-03-17 13:28:45 -0300

    Add program to calculate the square of a number

---
+++ questao7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i;
+    int quadrado;
+    printf("insira o numero:");
+    scanf("%d",&i);
+    quadrado = i*i;
+    printf("o quadrado do numero é: %d", quadrado);
+
+    return 0;
+}
